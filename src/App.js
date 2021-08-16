fc3fc1c816fbd0a81805268962d8dd0efb6a08a4
Muhammd-Kamil <maaz.alam00@gmail.com>
2021-08-16 11:58:08 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Resturant from './componenet/Basics/Resturant';
+
+function App(){
+  return(
+    <>
+  <Resturant/>
+    </>
+  )
+}
+export default App;
